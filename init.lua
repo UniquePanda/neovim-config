e2da0216aacf6c079c2408e88a36b7cf38131766
Marcel Wendler <marcel.wendler@pointcloudtechnology.com>
2024-07-09 13:11:18 +0200

    Add config for todo-comments plugin

--- init.lua
+++ init.lua
@@ -393,9 +393,6 @@ lazy.setup({
 		dependencies = {
 			'nvim-lua/plenary.nvim',
 		},
-		opts = {
-			signs = false,
-		}
 	},
 	-- Visual Clipboard History
 	{
@@ -447,6 +444,31 @@ require('neoclip').setup({
 				paste_behind = '<CR>',
 			},
 		},
+	},
+})
+
+require('todo-comments').setup({
+	keywords = {
+		FIX = {
+			icon = " ",
+			color = "error",
+			alt = { "FIXME", "BUG", "FIXIT", "ISSUE", 'fix', 'fixme', 'bug', 'fixit', 'issue' },
+		},
+		TODO = { icon = "", color = "info", alt = {'todo'} },
+		HACK = { icon = " ", color = "warning", alt = {'hack'} },
+		WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX", 'warn', 'warning', 'xxx' } },
+		PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", 'perf', 'optim', 'performance', 'optimize' } },
+		NOTE = { icon = " ", color = "hint", alt = { "INFO", 'note', 'info' } },
+		TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED", 'test', 'testing', 'passed', 'failed' } },
+	},
+	highlight = {
+		keyword = "bg",
+		before = "",
+		after = "bg",
+		pattern = [[.*<(KEYWORDS)\W]],
+	},
+	search = {
+		pattern = [[\b(KEYWORDS)\b]],
 	},
 })
 
