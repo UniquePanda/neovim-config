4bec06dd8063559963ac70759c83909a32495912
Marcel <ma-wendler@outlook.com>
2025-03-22 12:06:19 +0100

    Remove GitFugitive and some unused Git keybindings + update packages

--- init.lua
+++ init.lua
@@ -333,11 +333,6 @@ lazy.setup({
 		build = 'make'
 	},
 
-	-- # Git
-	{
-		-- Integration for Git
-		'tpope/vim-fugitive'
-	},
 	{
 		-- Git features (like highlighting new/modified/deleted lines, git blame, ...)
 		'lewis6991/gitsigns.nvim'
@@ -837,18 +832,5 @@ vim.keymap.set('n', '<leader>fw', telescope.grep_string, { desc = 'Find current 
 vim.keymap.set('n', '<leader>fg', telescope.git_files, { desc = 'Find files in current git repo' })
 
 -- Git
-local gitsigns = require('gitsigns')
--- Staging/Resetting hunks
-vim.keymap.set('v', '<leader>gs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Stage selected range' })
-vim.keymap.set('v', '<leader>gu', function() gitsigns.undo_stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Unstage selected range' })
-vim.keymap.set('v', '<leader>gr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Revert selected range' })
--- Staging/Resetting buffer
-vim.keymap.set({'n', 'v'}, '<leader>gS', gitsigns.stage_buffer, { desc = 'Stage all' })
-vim.keymap.set({'n', 'v'}, '<leader>gU', '<cmd>:Git restore --staged %<cr>', { desc = 'Unstage all' })
-vim.keymap.set({'n', 'v'}, '<leader>gR', gitsigns.reset_buffer, { desc = 'Revert all' })
--- Committing/Pushing
-vim.keymap.set({'n', 'v'}, '<leader>gc', '<cmd>:Git commit -a<cr>', { desc = 'Commit staged changes' })
-vim.keymap.set({'n', 'v'}, '<leader>gp', '<cmd>:Git push<cr>', { desc = 'Push new commits' })
-vim.keymap.set({'n', 'v'}, '<leader>gpf', '<cmd>:Git push --force<cr>', { desc = 'Force push new commits' })
 -- LazyGit (e.g. used for interactive rebases)
 vim.keymap.set({'n', 'v'}, '<leader>gl', '<cmd>LazyGitCurrentFile<cr>', {desc = 'Open LazyGit' })
