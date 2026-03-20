2523b899fcdf2b0409c58ab7a06f453ee5ffa8f5
Marcel Wendler <marcel.wendler@pointcloudtechnology.com>
2026-03-20 18:47:53 +0100

    Remove unused config for copilot LSP

--- init.lua
+++ init.lua
@@ -368,33 +368,6 @@ lazy.setup({
 	},
 
 	-- # Copilot
-	-- {
-	-- 	'copilotlsp-nvim/copilot-lsp',
-	-- 	init = function()
-	-- 		vim.g.copilot_nes_debounce = 500
-	-- 		vim.lsp.enable('copilot_ls')
-	-- 		vim.keymap.set('n', '<leader>j', function()
-	-- 			local bufnr = vim.api.nvim_get_current_buf()
-	-- 			local state = vim.b[bufnr].nes_state
-	-- 			if state then
-	-- 				-- Try to jump to the start of the suggestion edit.
-	-- 				-- If already at the start, then apply the pending suggestion and jump to the end of the edit.
-	-- 				local _ = require('copilot-lsp.nes').walk_cursor_start_edit()
-	-- 				or (
-	-- 					require('copilot-lsp.nes').apply_pending_nes()
-	-- 					and require('copilot-lsp.nes').walk_cursor_end_edit()
-	-- 				)
-	-- 				return nil
-	-- 			end
-	-- 		end, { desc = 'Accept Copilot NES suggestion', expr = true })
-	-- 		-- Clear copilot suggestion with Esc if visible, otherwise preserve default Esc behavior
-	-- 		vim.keymap.set('n', '<leader>k', function()
-	-- 			if not require('copilot-lsp.nes').clear() then
-	-- 				-- fallback to other functionality
-	-- 			end
-	-- 		end, { desc = 'Clear Copilot suggestion or fallback' })
-	-- 	end,
-	-- },
 	{
 		'zbirenbaum/copilot.lua',
 		cmd = 'Copilot',
